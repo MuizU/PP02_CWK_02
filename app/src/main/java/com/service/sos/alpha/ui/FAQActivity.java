18380c77a687e3b871e3a80c9c3d8ce42d56deb5
negeeshaKatulanda <anushasandyani1973@gmail.com>
2018-05-17 16:53:23 +0530

    origin

---
+++ app/src/main/java/com/service/sos/alpha/ui/FAQActivity.java
@@ -0,0 +1,23 @@
+package com.service.sos.alpha.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.service.sos.alpha.R;
+
+public class FAQActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faq);
+    }
+}
