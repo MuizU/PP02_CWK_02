761f1e590f722f01867d9ea4b3ac644a0daf87a2
MuizU <w1673601@my.westminster.ac.uk>
2018-05-17 15:48:17 +0530

    *Added buttons to the settings activity. *Added the invite friends option.
    Signed-off-by: MuizU <w1673601@my.westminster.ac.uk>

--- app/src/main/java/com/service/sos/alpha/ui/SettingsActivity.java
+++ app/src/main/java/com/service/sos/alpha/ui/SettingsActivity.java
@@ -1,6 +1,7 @@
 package com.service.sos.alpha.ui;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentTransaction;
@@ -11,15 +12,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.service.sos.alpha.MainActivity;
 import com.service.sos.alpha.R;
 
+import static android.content.Intent.FLAG_ACTIVITY_NEW_TASK;
+
 public class SettingsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle drawerToggle;
+    private TextView inviteFriends;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +41,17 @@ public class SettingsActivity extends AppCompatActivity implements NavigationVie
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         NavigationView navigationView = findViewById(R.id.nav_barSettings);
         navigationView.setNavigationItemSelectedListener(this);
+        inviteFriends = findViewById(R.id.inviteFriendsText);
+        inviteFriends.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent shareIntent = new Intent(Intent.ACTION_SEND);
+                shareIntent.setType("text/plain");
+                shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Assault Prevention Device");
+                shareIntent.putExtra(Intent.EXTRA_TEXT, "http://www.google.com");
+                startActivity(Intent.createChooser(shareIntent, "Share URL"));
+            }
+        });
 
 
     }
